b65e097a13eae526d12d947d00cc64c848145157
GabrielBatavia <gabrielbatavia7@gmail.com>
2025-11-17 06:56:55 +0700

    feat : add codelab 12

--- codelab11/books/lib/main.dart
+++ codelab11/books/lib/main.dart
@@ -31,13 +31,17 @@ class _FuturePageState extends State<FuturePage> {
   String result = "";
   bool loading = false;
 
-
+  // -------------------------------------------------------
+  // PRAKTIKUM 5 — returnError()
+  // -------------------------------------------------------
   Future returnError() async {
     await Future.delayed(const Duration(seconds: 2));
     throw Exception('Something terrible happened!');
   }
 
-
+  // -------------------------------------------------------
+  // PRAKTIKUM 5 — handleError()
+  // -------------------------------------------------------
   Future handleError() async {
     setState(() {
       loading = true;
@@ -45,19 +49,22 @@ class _FuturePageState extends State<FuturePage> {
     });
 
     try {
-      await returnError();
+      await returnError(); // ini akan melempar error
     } catch (error) {
       setState(() {
-        result = error.toString();     // tampilkan pesan error ke UI
+        result = error.toString();   // tampilkan error langsung ke UI
       });
     } finally {
-      print('Complete');               // selalu muncul di debug console
+      print('Complete');             // selalu tampil di debug console
       setState(() {
         loading = false;
       });
     }
   }
 
+  // -------------------------------------------------------
+  // UI
+  // -------------------------------------------------------
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,14 +76,20 @@ class _FuturePageState extends State<FuturePage> {
             ElevatedButton(
               child: const Text("Go!"),
               onPressed: () {
-                handleError();   // panggil method baru
+                handleError(); // Panggil method error handling
               },
             ),
 
             const SizedBox(height: 20),
-            Text(result, style: const TextStyle(fontSize: 22)),
+
+            Text(
+              result,
+              style: const TextStyle(fontSize: 22),
+              textAlign: TextAlign.center,
+            ),
 
             const SizedBox(height: 20),
+
             if (loading) const CircularProgressIndicator(),
           ],
         ),
